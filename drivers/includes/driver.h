8c0bf0eedb345b9fb980fe418dfb4e6cab7a6ae0
Zffu <103074097+Zffu@users.noreply.github.com>
2025-07-27 02:09:34 +0200

    docs: added docs

--- drivers/includes/driver.h
+++ drivers/includes/driver.h
@@ -9,8 +9,42 @@ typedef unsigned short word_t;
 
 typedef unsigned short port;
 
+/**
+ * @name port_get_byte
+ * 
+ * Get a byte from the specified port
+ * 
+ * @param port the port
+ * @return the byte
+ */
 byte_t port_get_byte(port port);
+
+/**
+ * @name port_put_byte
+ * 
+ * Puts a byte on the specified port
+ * 
+ * @param port the port
+ * @param data the byte to put
+ */
 void port_put_byte(port port, byte_t data);
 
+/**
+ * @name port_get_word
+ * 
+ * Gets a word from the specified port
+ * 
+ * @param port the port
+ * @return the word
+ */
 word_t port_get_word(port port);
+
+/**
+ * @name port_put_put
+ * 
+ * Puts a word on the specified port
+ * 
+ * @param port the port
+ * @param data the word to put
+ */
 void port_put_word(port port, word_t data);
