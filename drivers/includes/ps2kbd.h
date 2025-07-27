e61a4f3a5aee35df2f617587f75ad0ecdf60c226
Zffu <103074097+Zffu@users.noreply.github.com>
2025-07-28 01:22:02 +0200

    feat: added the rest of the function header

--- drivers/includes/ps2kbd.h
+++ drivers/includes/ps2kbd.h
@@ -19,5 +19,22 @@ typedef struct ps2kbd_led_state {
 	bool caps_lock;
 } ps2kbd_led_state_t;
 
+typedef enum ps2kbd_scanmode_t {
+	TYPEMATIC_AUTOREPEAT,
+	PRESS_RELEASE,
+	PRESS,
+	ALL
+} ps2kbd_scanmode_t;
+
 void ps2kbd_set_led_state(int led, bool state);
 void ps2kbd_push_led_state(ps2kbd_led_state_t ledstate);
+
+bool ps2kbd_echo();
+
+void ps2kbd_enable_scanning();
+void ps2kbd_disable_scanning();
+
+void ps2kbd_set_default_params();
+
+void ps2kbd_set_scanmode_global(ps2kbd_scanmode_t mode);
+void ps2kbd_set_scanmode_key(ps2kbd_scanmode_t mode, unsigned char key);
