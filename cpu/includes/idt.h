bcb5bbebb3625ab27732f31bb4b06a8a928c8b82
Zffu <103074097+Zffu@users.noreply.github.com>
2025-07-27 02:39:34 +0200

    feat: added CPU IDT

---
+++ cpu/includes/idt.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <types.h>
+
+#define KERNEL_CS 0x08
+
+typedef struct idt_gate_t {
+	u16 low_off;
+	u16 sel;
+	u8 always0;
+	u8 flags;
+	u16 high_off;
+} __attribute__((packed)) idt_gate_t;
+
+typedef struct idt_reg_t {
+	u16 limit;
+	u32 base;
+} __attribute((packed)) idt_reg_t;
+
+#define IDT_ENTRIES 256
+idt_gate_t idt[IDT_ENTRIES];
+idt_reg_t idt_reg;
+
+void set_idt_gate(int n, u32 handler);
+void set_idt();
