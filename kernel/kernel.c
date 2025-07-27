115476d9db16eebe82ecf6c695fc8ea4967a78d2
Zffu <103074097+Zffu@users.noreply.github.com>
2025-07-27 02:57:49 +0200

    feat: made keyboard load

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,8 +1,12 @@
 #include <screenprint.h>
 
+#include <isr.h>
+#include <keyboard/keyboard.h>
+
 void main() {
-	for(int i = 0; i < 25; ++i) {
-		screenprint((char) 56 + i);
-		screenprint("\n");
-	}
+	screenprint("[INFO] Installing the CPU ISR\n");
+	isr_install();
+
+	screenprint("[INFO] Loading Keyboard handler\n\n");
+	keyboard_init();
 }
