ea519b6066c7730c3ab608aa23a82a29576c830f
Zffu <103074097+Zffu@users.noreply.github.com>
2025-08-02 01:31:08 +0200

    feat: added taskio definitions

--- kernel/includes/taskio.h
+++ kernel/includes/taskio.h
@@ -6,16 +6,20 @@
 
 #include <types.h>
 
+#ifndef TASKIO_PERTASK_STACK_SIZE 
+#define TASKIO_PERTASK_STACK_SIZE 256
+#endif
+
 /**
  * Defines tasks that are fully 'internal', AKA tasks that do NOT need to be ticked 
  * trough the CPU timer interrupt
  */
 typedef struct taskio_task_t__bundled {
 	u8 id;
-	void *(detatch)();
+	void *(*detatch)();
 
-	struct taskio_task_t__bundled* next;
 	struct taskio_task_t__bundled* prev;
+	struct taskio_task_t__bundled* next;
 } internal_task_t;
 
 /**
@@ -29,4 +33,23 @@ typedef struct taskio_task_t {
 	u32 eax, ebx, ecx, edx;
 	u32 esi, edi;
 	u32 eflags;
+
+	struct taskio_task_t* prev;
+	struct taskio_task_t* next;
 } task_t;
+
+/**
+ * @name create_task
+ * 
+ * @param name the name of the task
+ * @param entry_point the entry point
+ */
+task_t* create_task(char* name, void (*entry_point)());
+
+/**
+ * @name create_internal_task
+ * 
+ * @param name the name of the internal task
+ * @param detach the function allowing to detach/kill the internal tasks
+ */
+internal_task_t* create_internal_task(char* name, void (*detach)());
---
+++ kernel/src/taskio.c
@@ -0,0 +1,50 @@
+#include <taskio.h>
+#include <memalloc.h>
+#include <mem.h>
+
+task_t* taskio_task_queue;
+internal_task_t* taskio_internaltask_queue;
+
+task_t* create_task(char* name, void (*entry_point)()) {
+	task_t* task = kmallocs(sizeof(task_t), 1);
+	task->stack = kmallocs(TASKIO_PERTASK_STACK_SIZE, 1);
+
+	u32 top = (u32) task->stack + TASKIO_PERTASK_STACK_SIZE;
+
+	#ifdef TASKIO_EMPTY_STACK
+	memset(task->stack, 0, TASKIO_PERTASK_STACK_SIZE);
+	#endif
+
+	task->esp = top - 16;
+	task->ebp = task->esp;
+	task->eip = (u32) entry_point;
+
+	task->next = taskio_task_queue;
+
+	if(!taskio_task_queue) {
+		taskio_task_queue = task;
+		task->next = task;
+	}
+	else {
+		taskio_task_queue->prev = task;
+		task->next = taskio_task_queue;
+		taskio_task_queue = task;
+	}
+}
+
+internal_task_t* create_internal_task(char* name, void (*detach)()) {
+	internal_task_t* task = kmallocs(sizeof(task_t), 1);
+
+	task->next = taskio_internaltask_queue;
+	task->detatch = detach;
+
+	if(!taskio_internaltask_queue) {
+		taskio_internaltask_queue = task;
+		task->next = task;
+	}
+	else {
+		taskio_internaltask_queue->prev = task;
+		task->next = taskio_internaltask_queue;
+		taskio_internaltask_queue = task;
+	}
+}
