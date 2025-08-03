0fcc7ad53dfa3fbe09e6d9d5b10beefb513a468f
Zffu <103074097+Zffu@users.noreply.github.com>
2025-08-03 20:32:38 +0200

    feat: added IOPORT_REQ interrupt

--- kernel/src/syscall/syscall.c
+++ kernel/src/syscall/syscall.c
@@ -1,11 +1,31 @@
 #include <klib/syscall.h>
 #include <syscall.h>
+#include <io/ioports.h>
+
+#include <types.h>
 
 syscall_response khandle_syscall(syscall call, SYSCALL_ARGBUFF argbuff) {
     switch(call) {
         case PRINT:
             return DENIED_STRICT;
+
         case IOPORT_REQ:
+            if(argbuff == SYSCALL_NOARGS) return INVALID_SYSCALL;
+            u8* ptr = (u8*) argbuff;
+
+            u8 port = *ptr;
+            u8 mode = *(ptr + 1);
+
+            if(port < 0) return INVALID_SYSCALL;
+            if(mode < 0 || mode > 1) return INVALID_SYSCALL;
+
+            if(!port_is_allowed(port)) return DENIED;
+
+            // TODO: check for driver permissions
+            break;
         
+        
+
+            
     }
 }
