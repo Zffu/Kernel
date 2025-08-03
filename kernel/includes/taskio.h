86e1570d79d9c9f3feb500157b72413d89a3ce96
Zffu <103074097+Zffu@users.noreply.github.com>
2025-08-03 21:01:46 +0200

    feat: added taskio::task_kill_instant declaration

--- kernel/includes/taskio.h
+++ kernel/includes/taskio.h
@@ -74,3 +74,15 @@ internal_task_t* create_internal_task(char* name, void (*detach)());
  * @param tree the tree pointer of the tasks. 
 */
 TASKIO_TASK_LIKELY_POINTER find_task(char* name, TASKIO_TASK_LIKELY_POINTER tree);
+
+/**
+ * @name task_kill_instant
+ *
+ * Kills the task if it exists, skips every permission / authority check when killing the task, doesn't wait for the app to be ready.
+ *
+ * @param name the name of the task
+ * @param tree the tree pointer of the tasks.
+ * @param mode the mode, 0x00 if normal task, 0x01 if internal
+ * @return 0x00 if the task couldn't be killed, 0x01 if it was
+ */
+u8 task_kill_instant(char* name, TASKIO_TASK_LIKELY_POINTER tree, u8 mode);
