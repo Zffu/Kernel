f5881147b10064d44a5febcc068e491f73c2bb49
Zffu <103074097+Zffu@users.noreply.github.com>
2025-08-03 20:38:55 +0200

    feat: added TASk_KILL syscall

--- kernel/src/syscall/syscall.c
+++ kernel/src/syscall/syscall.c
@@ -2,7 +2,14 @@
 #include <syscall.h>
 #include <io/ioports.h>
 
+#include <taskio.h>
+
+#include <str.h>
+
 #include <types.h>
+
+extern internal_task_t* taskio_internaltask_queue;
+extern task_t* taskio_task_queue;
 
 syscall_response khandle_syscall(syscall call, SYSCALL_ARGBUFF argbuff) {
     switch(call) {
@@ -24,8 +31,45 @@ syscall_response khandle_syscall(syscall call, SYSCALL_ARGBUFF argbuff) {
             // TODO: check for driver permissions
             break;
         
-        
+        case TASK_KILL:
+            if(argbuff == SYSCALL_NOARGS) return INVALID_SYSCALL;
+            u8* ptr = (u8*) argbuff;
 
+            u8 task_type = *ptr;
+            char* task_name = (char*) (ptr + 1);
+
+            if(task_type < 0 || task_type > 1) return INVALID_SYSCALL;
+            if(task_name == 0) return INVALID_SYSCALL;
+
+            // Internal
+            if(task_type == 0x01) {
+                internal_task_t* node = taskio_internaltask_queue;
+
+                while(node != 0) {
+                    if(strcmp(task_name, node->name)) {
+                        node->detach();
+
+                        return ACCEPTED;
+                    }
+
+                    node = node->next;
+                }
+
+                return ACCEPT_ERR;
+            }
             
+            task_t* node = taskio_task_queue;
+
+            while(node != 0) {
+                if(strcmp(task_name, node)) {
+                    // TODO: remove node
+
+                    return ACCEPTED;
+                }
+
+                node = node->next;
+            }
+
+            return ACCEPT_ERR;
     }
 }
